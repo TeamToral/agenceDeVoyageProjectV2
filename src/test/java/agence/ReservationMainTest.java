cb6b261077f125b310303f3f6eae9e487aaa1d4f
camille <tal.adoum@gmail.com>
2017-05-04 15:34:50 +0200

    Reservation CRUD et MainResa

--- src/test/java/agence/ReservationMainTest.java
+++ src/test/java/agence/ReservationMainTest.java
@@ -1,10 +1,14 @@
 package agence;
 
+import java.util.Date;
 import java.util.List;
 
+import agence.dao.PassagerDaoSql;
 import agence.dao.ReservationDao;
 import agence.dao.ReservationDaoSql;
+import agence.model.Passager;
 import agence.model.Reservation;
+
 
 public class ReservationMainTest {
 
@@ -13,19 +17,32 @@ public class ReservationMainTest {
 		// J'instancie le dao qui va récupérer à ma place les données
         // dans la BDD
         ReservationDao reservationDao = new ReservationDaoSql();
+        PassagerDaoSql passagerDao = new PassagerDaoSql();
         // Ma liste d'élèves qui va contenir la liste de résultats
         List<Reservation> listeDreservations;
         // je demande au DAO de me trouver tous les élèves de la BDD
-        listeDreservations = reservationDao.findAll();  
-        // mettre à jour l'élève n°3, je change sa note
-        Reservation reservationAChanger = reservationDao.findById(54);
-        // je mets à jour au niveau de l'OBJET la note
-        reservationAChanger.setNumero("2222");
-        // je mets à jour au niveau BDD !!
-        Reservation reservationMiseAJour = reservationDao.update(reservationAChanger);
-        // Je supprime l'élève n°5
-        Reservation reservationASupprimer = reservationDao.findById(54);
-        reservationDao.delete(reservationASupprimer);
+        listeDreservations = reservationDao.findAll(); 
+        
+        Passager passager = passagerDao.findById(1);
+        
+        Reservation tokyo = new Reservation();
+        tokyo.setNumero("123456");
+        tokyo.setPassager(passager);
+        tokyo.setIdCli(10);
+		
+		reservationDao.create(tokyo);
+        
+        
+//        // mettre à jour l'élève n°3, je change sa note
+//        Reservation reservationAChanger = reservationDao.findById(55);
+//        // je mets à jour au niveau de l'OBJET la note
+//        reservationAChanger.setNumero("111");
+//        // je mets à jour au niveau BDD !!
+//        Reservation reservationMiseAJour = reservationDao.update(reservationAChanger);
+//        // Je supprime l'élève n°5
+//        Reservation reservationASupprimer = reservationDao.findById(55);
+//        reservationDao.delete(reservationASupprimer);
+        
 	}
 
 }
